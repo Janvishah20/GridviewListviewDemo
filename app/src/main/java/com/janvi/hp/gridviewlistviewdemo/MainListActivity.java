71e609a1d886e7b4508bee3902fac633a7403122
harshshah456 <harshshahkhs456@gmail.com>
2017-02-14 21:38:08 -0800

    Girdview Listview commit

---
+++ app/src/main/java/com/janvi/hp/gridviewlistviewdemo/MainListActivity.java
@@ -0,0 +1,47 @@
+package com.janvi.hp.gridviewlistviewdemo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list);
+        TextView textView;
+
+        textView=(TextView)findViewById(R.id.txtvw6);
+
+        String quotes;
+        Intent intent=getIntent();
+        quotes=intent.getStringExtra("pos");
+
+        textView.setText(quotes);
+
+
+        Button btn;
+        btn=(Button)findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                sharingIntent.setType("text/html");
+                sharingIntent.putExtra(Intent.EXTRA_TEXT, Html.fromHtml("<p>This is the text that will be shared.</p>"));
+                startActivity(Intent.createChooser(sharingIntent, "Share using"));
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/janvi/hp/gridviewlistviewdemo/Post.java
@@ -0,0 +1,24 @@
+package com.janvi.hp.gridviewlistviewdemo;
+
+
+public class Post {
+
+    int id;
+    String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
